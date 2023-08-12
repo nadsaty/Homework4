9a98ba1287585862a96abbbc84b684516202281e
nadsay <nadsatyyy@gmail.com>
2023-08-12 13:56:08 +0300

    Домашка 4, Задача 2

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,29 @@
 public class Main {
     public static void main(String[] args) {
-        int age = 19;
-        System.out.printf("Если возраст человека равен " + age + ", то он ");
-        if (age >= 18) {
-            System.out.printf("совершеннолетний");
-        } else {
-            System.out.printf("не достиг совершеннолетия, нужно немного подождать");
+        task1();
+        task2();
+    }
+        public static void task1 () {
+            System.out.println("Задача 1 \n");
+            int age = 19;
+            System.out.println("Если возраст человека равен " + age + ", то он ");
+            if (age >= 18) {
+                System.out.println("совершеннолетний \n");
+            } else {
+                System.out.println("не достиг совершеннолетия, нужно немного подождать \n");
+            }
         }
 
+        public static void task2 () {
+            System.out.println("Задача 2 \n");
+            int temperature = 7;
+            System.out.println("На улице " + temperature + " градусов, ");
+            if (temperature < 5) {
+                System.out.println("нужно надеть шапку");
+            } else if (temperature >= 5) {
+                System.out.println("можно идти без шапки");
+            }
+
+
+        }
     }
-}
