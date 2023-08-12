5ae742cb6448317c655b4403f75628cfc93d0bd3
nadsay <nadsatyyy@gmail.com>
2023-08-12 14:31:49 +0300

    Домашка 4, Задача 4

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
+        task4();
     }
         public static void task1 () {
             System.out.println("Задача 1 \n");
@@ -22,6 +24,38 @@ public class Main {
                 System.out.println("нужно надеть шапку \n");
             } else if (temperature >= 5) {
                 System.out.println("можно идти без шапки \n");
+            }
+        }
+
+
+        public static void task3 () {
+            System.out.println("Задача 3 \n");
+            int speed = 40;
+            System.out.println("Если скорость " + speed + " км/час, то ");
+            if (speed > 60) {
+                System.out.println("придется заплатить штраф \n");
+            } else if (speed <= 60) {
+                System.out.println("можно ездить спокойно \n");
+        }
+    }
+
+
+        public static void task4 () {
+            System.out.println("Задача 4 \n");
+            int howOld = 55;
+            System.out.println("Если возраст человека равен " + howOld + ", то ");
+            if ( howOld < 2) {
+                System.out.println("ему пора спать \n");
+            } else if (howOld >= 2 && howOld <= 6) {
+                System.out.println("ему нужно ходить в детский сад \n");
+            } else if (howOld >= 7 && howOld < 18) {
+                System.out.println("ему нужно ходить в школу \n");
+            } else if (howOld > 18 && howOld < 24) {
+                System.out.println("ему нужно ходить в университет \n");
+            } else if (howOld >= 24 && howOld < 60) {
+                System.out.println("ему пора ходить на работу \n");
+            } else if (howOld > 60) {
+                System.out.println("он может отдохнуть \n");
             }
         }
 
